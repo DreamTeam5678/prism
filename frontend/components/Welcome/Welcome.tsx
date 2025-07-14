24ce8c3c9f32b5a2d749f79c9e3bf8e69f352707
Senaf Geleta <senafg19@gmail.com>
2025-07-14 11:48:08 -0700

    Updating Welcome Screen UI

--- frontend/components/Welcome/Welcome.tsx
+++ frontend/components/Welcome/Welcome.tsx
@@ -4,12 +4,23 @@ import { useSession } from "next-auth/react";
 
 export default function Welcome() {
     return (
-        <div className="welcome-container">
-            <h1 className="welcome-title">Welcome to <img className="logo" src="./logo.png" alt="Prism Logo"/></h1>
-            <button className="log-in-button" onClick={() => window.location.href = "/home"}>
-                Log In
-            </button>
-        </div>
+        <body>
+            <div className="welcome-container">
+                <h1 className="welcome-title">welcome to </h1>
+                <div className ="check-container">
+                <img className="logo" src="./logo.png" alt="Prism Logo"/>
+                <div className="checkbox-wrapper">
+                    <svg className="checkbox" viewBox="0 0 52 52">
+                        <circle className="checkbox-circle" cx="26" cy="26" r="25" fill="none" />
+                        <path className="checkbox-check" fill="none" d="M14 27l7 7 16-16" />
+                    </svg>
+                </div>
+                </div>
+                <button className="log-in-button" onClick={() => window.location.href = "/home"}>
+                    Log In
+                </button>
+            </div>
+        </body>
     );
 }
 
