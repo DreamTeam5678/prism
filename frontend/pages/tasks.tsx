8e8a053d0f6e33da8cfc8daab1f156ecd07f3aa1
Senaf Geleta <senafg19@gmail.com>
2025-07-21 10:54:41 -0700

    undoing migration

--- frontend/pages/tasks.tsx
+++ frontend/pages/tasks.tsx
@@ -1,6 +1,12 @@
 import TaskList from "../components/TasksPage/TaskList";
 
+<<<<<<< Updated upstream
 
 export default function TasksPage() {
     return <TaskList />
 }
+=======
+export default function TasksPage() {
+    return <TaskList />
+}
+>>>>>>> Stashed changes
