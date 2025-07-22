8402fdf6a5dffbfef864b47d9b515575fb5beb61
Senaf Geleta <senafg19@gmail.com>
2025-07-22 16:08:33 -0700

    reinclusion of imports

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,19 @@
+import "../components/CalendarPage/Calendar/CalendarPage.css";
+
+// pages/_app.tsx
+import "../components/Welcome/Welcome.css"
 import "../components/CalendarPage/Upcoming/Upcoming.css";
 import "../styles/globals.css";
 import "../components/CalendarPage/Calendar/CalendarPage.css";
+import "../components/CalendarPage/Upcoming/Upcoming.css";
+import '../styles/globals.css';
 import "../components/NavBar/NavBar.css";
 import "../components/CalendarPage/Optimize/Optimize.module.css";
 import "../components/TasksPage/TaskList.css";
 import "../components/FlowSpace/YoutubePlayer.css";
 import "../components/MoodSelector/MoodModal.css";
 import "../components/FlowSpace/BackgroundPicker.css";
+
 
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
