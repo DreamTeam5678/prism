c84018364512e8baad23a07b69b99ae0e7e57a9d
Lauritz Patterson <lauritzpatterson31@yahoo.com>
2025-07-22 15:23:10 -0700

    Add shared XP context and synced XP bar to Task and Calendar pages

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,25 +1,22 @@
-// pages/_app.tsx
-import "../components/CalendarPage/Calendar/CalendarPage.css";
-import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
-// pages/_app.tsx
-import "../components/Welcome/Welcome.css"
 import "../components/CalendarPage/Upcoming/Upcoming.css";
 import "../styles/globals.css";
 import "../components/CalendarPage/Calendar/CalendarPage.css";
-import "../components/CalendarPage/Upcoming/Upcoming.css";
-import '../styles/globals.css';
 import "../components/NavBar/NavBar.css";
 import "../components/CalendarPage/Optimize/Optimize.module.css";
 import "../components/TasksPage/TaskList.css";
-
 import "../components/MoodSelector/MoodModal.css";
-
 import "../components/FlowSpace/BackgroundPicker.css";
+
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { XPProvider } from "@/context/XPContext"; // ✅ Import your shared XP context
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <XPProvider> {/* ✅ Provide XP and task state to all pages */}
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </XPProvider>
   );
 }
--- frontend/pages/calendar.tsx
+++ frontend/pages/calendar.tsx
@@ -1,8 +1,17 @@
+"use client";
+import { useXP } from "@/context/XPContext";
 import CalendarPage from "../components/CalendarPage/Calendar/CalendarPage";
-
+import XPBar from "@/components/XPBar";
 
 export default function Calendar() {
-    return <CalendarPage /> 
-    ;
+  const { xp } = useXP();
 
+  return (
+    <div style={{ position: "relative" }}>
+      <div style={{ position: "absolute", top: "1rem", right: "1rem", zIndex: 10 }}>
+        <XPBar xp={xp} />
+      </div>
+      <CalendarPage />
+    </div>
+  );
 }
