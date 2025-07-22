3c7f7fe58ac40272a9dc065239ae55dd8f278ebe
Senaf Geleta <senafg19@gmail.com>
2025-07-22 11:23:21 -0700

    updating background selection logic

--- frontend/components/FlowSpace/BackgroundPicker.tsx
+++ frontend/components/FlowSpace/BackgroundPicker.tsx
@@ -1,8 +1,15 @@
+
+import { useState, useRef, useEffect } from "react";
+
 const backgrounds = [
-   'https://i.pinimg.com/originals/08/e5/97/08e597e1addb5f2aab9ae3ac4820b285.gif',
-    'https://i.pinimg.com/originals/08/e5/97/08e597e1addb5f2aab9ae3ac4820b285.gif',
-    'https://i.pinimg.com/originals/08/e5/97/08e597e1addb5f2aab9ae3ac4820b285.gif',
-    'https://images.unsplash.com/photo-1509228468518-180dd4864904?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWF1cjl1cHlud2JrYm5kcjE5Y2ZkYjZkMWwzOGZqZnM1dDhzNGR4ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d3PcEfjRsLUqgTGZSE/giphy.gif',
+   'https://i.pinimg.com/originals/9c/d1/a0/9cd1a09bb5c3d5a6774128147c96b18b.gif',
+    'https://wallpaperaccess.com/full/2641074.gif',
+    'https://cdna.artstation.com/p/assets/images/images/059/104/894/original/ryan-haight-delridge-large-gif.gif?1675666673',
+    'https://i.pinimg.com/originals/a4/04/a4/a404a481f48045b1a24cdbba5cc8d350.gif',
+    'https://i.pinimg.com/originals/bb/d8/5f/bbd85fa86d8dc8e3fc64d086f6641a5c.gif',
+    'https://wallpapers-clan.com/wp-content/uploads/2024/03/starfall-night-sky-mountains-aesthetic-gif-preview-desktop-wallpaper.gif'
+
 ];
 
 export default function BackgroundPicker({current, onChange}) {
@@ -14,7 +21,7 @@ export default function BackgroundPicker({current, onChange}) {
                     className={`background-picker-item ${current === index ? "selected" : ""}`}
                     onClick={() => onChange(index)}
                 >                    
-                    <img src={url} alt="Background" />
+                    <img src={url} alt={`Background ${index + 1}`} />
                 </div>
             ))}
         </div>
--- frontend/pages/flowspace.tsx
+++ frontend/pages/flowspace.tsx
@@ -2,15 +2,43 @@ import NavBar from "@/components/NavBar/NavBar";
 import Avatars from "../components/FlowSpace/Avatars";
 import BackgroundPicker from "../components/FlowSpace/BackgroundPicker";
 import SpotifyPlayer from "../components/FlowSpace/SpotifyPlayer";
+import { useState } from "react";
+
+const backgrounds = [
+  'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWF1cjl1cHlud2JrYm5kcjE5Y2ZkYjZkMWwzOGZqZnM1dDhzNGR4ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d3PcEfjRsLUqgTGZSE/giphy.gif',
+   'https://i.pinimg.com/originals/9c/d1/a0/9cd1a09bb5c3d5a6774128147c96b18b.gif',
+    'https://wallpaperaccess.com/full/2641074.gif',
+    'https://cdna.artstation.com/p/assets/images/images/059/104/894/original/ryan-haight-delridge-large-gif.gif?1675666673',
+    'https://i.pinimg.com/originals/a4/04/a4/a404a481f48045b1a24cdbba5cc8d350.gif',
+    'https://i.pinimg.com/originals/bb/d8/5f/bbd85fa86d8dc8e3fc64d086f6641a5c.gif',
+    'https://wallpapers-clan.com/wp-content/uploads/2024/03/starfall-night-sky-mountains-aesthetic-gif-preview-desktop-wallpaper.gif'
+
+];
 
 export default function FlowSpacePage() {
+  const [selected, setSelected] = useState<number>(0);
+  
   return (
-    <>
+    <div style={{
+      backgroundImage: `url(${backgrounds[selected]})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      minHeight: "105vh",
+      transition: "background-image 0.5s ease-in-out",
+      marginTop: "-10px",
+      marginBottom: "-10px",
+      opacity: 0.8,
+    }}>
+    
       <NavBar />
       <Avatars />
-      <BackgroundPicker />
-      <SpotifyPlayer />
-    </>
+      <BackgroundPicker
+        current= {selected}
+        onChange={setSelected}
+       />
+     
+      </div>
+    
     
   );
 
